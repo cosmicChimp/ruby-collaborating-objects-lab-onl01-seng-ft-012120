ae788bb46ef18cca1b5782966a0dc73d9fa7502d
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-04 23:58:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,19 +2,54 @@ require 'pry'
 
 class Artist 
   attr_accessor :name, :songs
-  
-  @@all = []
-  
-  def initialize(name)
+
+  @@all = [] 
+
+  def initialize(name)#artist_name
     @name = name
-    @songs = []
-    
+    @songs = [] 
   end
-  
+
   
   def self.all
-    @all
+    @@all
   end
-  
+
+  def add_song(song)
+    
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
+
+
+
+  def save
+    @@all << self
+  end
+
+  def print_songs()
+    puts @songs.collect {|x| x.name}
+  end
+
 end
 
