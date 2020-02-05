f674e15ecaa74eb30397561b9f468a75ca699c16
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-05 00:44:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,19 +24,17 @@ class Artist
   
 
   def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+    new_var = Artist.all.find {|x| x.name == name}
+    if new_var.nil?
+      new_var = Artist.new(name)
   end
 
 
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
+  #def self.find(name)
+    #@@all.find do |artist|
+     # artist.name == name
+    #end
+  #end
 
   def self.create(name)
     artist = self.new(name)
