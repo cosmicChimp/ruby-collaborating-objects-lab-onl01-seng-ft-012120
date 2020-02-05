f8078cc10b9de1b2d77ba3ccbe467aa6fd27a065
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-05 00:12:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,12 @@ class Song
 
   attr_accessor :name, :artist
   
+  @@all = []
 
   def initialize(name)
     @name = name
+    @artist = artist
+    @@all << self
   end
 
   def self.new_by_filename(file)
